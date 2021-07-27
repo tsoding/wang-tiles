34ba6c5a6638a70aaa34e188515ede0062c868fd
rexim <reximkut@gmail.com>
2021-07-27 18:42:03 +0700

    Introduce src folder

--- nobuild.c
+++ nobuild.c
@@ -16,16 +16,16 @@ int main(int argc, char **argv)
 
     if (argc > 1) {
         if (strcmp(argv[1], "run") == 0) {
-            CMD(cc(), CFLAGS, "-o", "wang", "main.c", LIBS);
+            CMD(cc(), CFLAGS, "-o", "wang", "src/main.c", LIBS);
             CMD("./wang");
         } else if (strcmp(argv[1], "gdb") == 0) {
-            CMD(cc(), CFLAGS, "-o", "wang", "main.c", LIBS);
+            CMD(cc(), CFLAGS, "-o", "wang", "src/main.c", LIBS);
             CMD("gdb", "./wang");
         } else {
             PANIC("Unknown subcommand %s", argv[1]);
         }
     } else {
-        CMD(cc(), CFLAGS, "-o", "wang", "main.c", LIBS);
+        CMD(cc(), CFLAGS, "-o", "wang", "src/main.c", LIBS);
     }
     return 0;
 }
