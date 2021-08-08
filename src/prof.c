0eb23ef514b45ee3d4f187a4fdaf0536d1551900
rexim <reximkut@gmail.com>
2021-08-09 04:23:39 +0700

    [profiler] factor out clear_summary()

--- src/prof.c
+++ src/prof.c
@@ -137,13 +137,17 @@ size_t estimate_line_width(void)
     return line_width;
 }
 
+void clear_summary()
+{
+    clock_stack_count = 0;
+    summary_count = 0;
+}
+
 void dump_summary(FILE *stream)
 {
     size_t line_width = estimate_line_width();
     render_summary(stream, line_width + 2);
-
-    clock_stack_count = 0;
-    summary_count = 0;
+    clear_summary();
 }
 
 #else
